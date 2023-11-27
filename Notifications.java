2346640ddd617ff30017aa3323dec9811f8b234f
Otatopot <rochelewebs24@gmail.com>
2023-11-27 15:50:52 -0500

    Created Notifications and Registration Classes
    This is just the spine of the class. Just the basic outline that has all the attributes and methods displayed

---
+++ Notifications.java
@@ -0,0 +1,29 @@
+
+import java.util.ArrayList;
+
+public class Notifications {
+    private String subject;
+    private String body;
+    private String Sender;
+    private String text;
+    private String systemNotification;
+    private ArrayList<String> recipients;
+    private String schedule; //should be Schedule class
+
+    public void generateEmailNotification() {}
+
+    public void sendEmailNotification() {}
+
+    public void generateTextNotification() {}
+
+    public void sendTextNotification() {}
+    
+    public void identifyRecipients() {}
+
+    public void generateSystemNotifications() {}
+
+    public void displaySystemNotifications() {}
+
+
+
+}
---
+++ Registration.java
@@ -0,0 +1,17 @@
+
+class Registration{
+    private String schedule;
+
+    public void makeRegistration() {}
+
+    public void cancelRegistration() {}
+
+    public void requestEmailNotifications() {}
+
+    public void requestTextNotifications() {}
+
+    public void openRegistration() {}
+
+    public void closeRegistration() {}
+
+}
